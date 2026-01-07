71cd3032bf26f5763e53a8430fa527724702f5f0
SoumilMalik24 <soumil4malik@gmail.com>
2026-01-07 13:03:07 +0530

    implementation of gemini api using openai lib

---
+++ gemini_using_openai.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/"
+)
+
+
+response = client.chat.completions.create(
+    model="gemini-2.5-flash",
+    n=1,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "Explain to me how AI works"
+        }
+    ]
+)
+
+print(response.choices[0].message)
